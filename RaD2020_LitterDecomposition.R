59848aba0940a7022abd8f7b1fb7e73151faf49d
Laura Y. Podzikowski <36493186+Lpodziko@users.noreply.github.com>
2023-08-14 10:27:07 -0500

    Add files via upload

--- RaD2020_LitterDecomposition.R
+++ RaD2020_LitterDecomposition.R
@@ -1,9 +1,10 @@
 #Load Packages-------
 library(lme4)
+library(lmerTest)
 
 #Load Data-------
 FileName<-"SITable_k.csv"
-WorkDir<-"~/"
+WorkDir<-"~/Dropbox/KU/Dimensions/aManuscriptPreparations/LitterDecomp-Dim4a-2020"
 kdf<-read.csv(file=paste(WorkDir,FileName,sep="/"))
 remove(FileName,WorkDir)
 
@@ -18,54 +19,135 @@ kdf$TwoSpp<-as.factor(kdf$TwoSpp)
 kdf$TwoSppFab<-as.factor(kdf$TwoSppFab)
 
 levels(kdf$CommComp)
-kdf$CommComp <- 
+kdf$CommComp.rl <- 
   relevel(kdf$CommComp, ref = "OVER")
+str(kdf)
 
 kdf2<-subset(kdf,Richness==2)
 
 kdfmix<-subset(kdf,Richness!=1)
 
-#LME 1: Richness, CommComp, Rain, Prop Panvir-------
+kdfmixPOA<-subset(kdfmix,CommComp=="POA")
 
-anova(kd4lmermod<-lmer(k~Richness*CommComp*Rain
-                       +PropPanvir
+kdfmixFAB<-subset(kdfmix,CommComp=="FAB")
+
+#Experimental Design models-------
+##LME 1: Richness, CommComp, Rain, Prop Panvir-------
+
+anova(lme1<-lmer(k~Richness*CommComp.rl*Rain
+                       +PropPanvir.Rlzd
                        +(1|Subblock)
                        ,data=kdf))
-summary(kd4lmermod)
-rand(kd4lmermod)
+summary(lme1)
+rand(lme1)
 
-#LME 2: Richness, Rain, Phylo Dist from Panvir-------
+##LME 2: Richness, Rain, Phylo Dist from Panvir-------
 
-anova(kd4lmermod.Phylo<-lmer(k~Richness*Rain
+anova(lme2<-lmer(k~Richness*Rain
                          +PhyloDistPv
                          +(1|Subblock)
                          ,data=kdf))
 
-summary(kd4lmermod.Phylo)
-rand(kd4lmermod.Phylo)
+summary(lme2)
+rand(lme2)
 
-#Two Species Mixture ANOVA--------
+##AOV 1: Two Species Mixture ANOVA--------
 
-kdf2aov2spp<-aov(k~TwoSpp,data=kdf2)
-
-par(mfrow=c(1,2))
-plot(kdf2aov2spp,which=c(1,2))
-shapiro.test(resid(kdf2aov2spp))
-
-anova(kdf2aov2spp)
-summary(kdf2aov2spp)
-TukeyHSD(kdf2aov2spp)
-
-
-#Multiple Species Mixtures three-way ANOVA--------
-
-kdfpresabsmod<-aov(k~Fab10+Poa10+Ast10,data=kdfmix)
+aov1<-aov(k~TwoSpp,data=kdf2)
 
 par(mfrow=c(1,2))
-plot(kdfpresabsmod,which=c(1,2))
-shapiro.test(resid(kdfpresabsmod))
+plot(aov1,which=c(1,2))
+shapiro.test(resid(aov1))
 
-anova(kdfpresabsmod)
-summary(kdfpresabsmod)
-TukeyHSD(kdfpresabsmod)
+anova(aov1)
+summary(aov1)
+TukeyHSD(aov1)
+
+
+##AOV 2: Multiple Species Mixtures three-way ANOVA--------
+
+aov2<-aov(k~Fab10+Poa10+Ast10,data=kdfmix)
+
+par(mfrow=c(1,2))
+plot(aov2,which=c(1,2))
+shapiro.test(resid(aov2))
+
+anova(aov2)
+summary(aov2)
+TukeyHSD(aov2)
+
+#Microbial Community-------
+##LME 3-6: Microbial Diversity-----
+
+lme3<-lmer(k~ShannonsH.FungSapr+(1|Subblock),data=kdf)
+anova(lme3)
+summary(lme3)
+
+lme4<-lmer(k~Richness.FungSapr+(1|Subblock),data=kdf)
+anova(lme4)
+summary(lme4)
+
+lme5<-lmer(k~ShannonsH.Bacteria+(1|Subblock),data=kdf)
+anova(lme5)
+summary(lme5)
+
+lme6<-lmer(k~Richness.Bacteria+(1|Subblock),data=kdf)
+anova(lme6)
+summary(lme6)
+
+##Piecewise Reg: Microbial Dissimilarity from switchgrass-----
+
+piecewise1 <- lm(k ~ AvgDistFung*(PropPanvir.Pntd > 0) + AvgDistFung*(PropPanvir.Pntd == 0),data=kdf)
+anova(piecewise1)
+summary(piecewise1)
+
+piecewise2 <- lm(k ~ AvgDistBact*(PropPanvir.Pntd > 0) + AvgDistBact*(PropPanvir.Pntd == 0),data=kdf)
+anova(piecewise2)
+summary(piecewise2)
+
+#Litter Chemistry-------
+##LME 7: N difference--------
+
+trnsno<-(-1*floor(min(kdf$Ndiff,na.rm=T)))
+lme7<-lmer(log(Ndiff+trnsno)~scale(k)*CommComp+(1|Subblock),data=kdf)
+
+anova(lme7)
+summary(lme7)
+rand(lme7)
+remove(trnsno)
+
+##LME 8: C difference--------
+
+trnsno<-(-1*floor(min(kdf$Cdiff,na.rm=T)))
+lme8<-lmer(log(Cdiff+trnsno)~scale(k)*CommComp+(1|Subblock),data=kdf)
+
+anova(lme8)
+summary(lme8)
+rand(lme8)
+remove(trnsno)
+
+##LME 9: CN difference--------
+
+lme9<-lmer(CNdiff~scale(k)*CommComp+(1|Subblock),data=kdf)
+
+anova(lme9)
+summary(lme9)
+rand(lme9)
+
+#Overyielding by k-------
+##LME 10: Grasses----
+
+lme10<-lmer(CE~scale(k)*PropPanvir.Pntd+(1|Subblock),data=kdfmixPOA)
+
+anova(lme10)
+summary(lme10)
+rand(lme10)
+
+##LME 11: Legumes----
+
+lme11<-lmer(CE~scale(k)+(1|Subblock),data=kdfmixFAB)
+
+anova(lme11)
+summary(lme11)
+rand(lme11)
 
